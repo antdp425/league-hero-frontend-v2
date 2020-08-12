600d81094a2e880a8139e8ad8d4a01eedb3e9018
antdp425 <antdp425@aol.com>
2020-08-12 07:20:26 -0400

    adds bootstrap

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import thunk from "redux-thunk"
 import {rootReducer} from './reducers'
 import App from './App';
 import './index.css'
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)))
 
