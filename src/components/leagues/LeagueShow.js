93790f2590d15ed5af53edf9000bada6ddf2208a
antdp425 <antdp425@aol.com>
2020-08-12 19:35:54 -0400

    adds formatting to LeagueShow, as well as Edit and Delete buttons

--- src/components/leagues/LeagueShow.js
+++ src/components/leagues/LeagueShow.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, ButtonGroup, Button } from 'react-bootstrap'
 
 
 function LeagueShow({match, leagues}) {
 
    const league = leagues.find(league => league.id == match.params.leagueId)
+
    let unformattedStart = league && league.start_date
    let start = new Date(unformattedStart)
    let formattedStart = new Date(`${start.getFullYear()}-0${start.getMonth()+1}-${start.getUTCDate()+1}`)
+
    let unformattedEnd = league && league.end_date
    let end = new Date(unformattedEnd)
    let formattedEnd = new Date(`${end.getFullYear()}-0${end.getMonth()+1}-${end.getUTCDate()+1}`)
@@ -21,11 +23,19 @@ function LeagueShow({match, leagues}) {
                <Card.Body className="league-show-card">
                   <Card.Title className="league-show-card-title">{league.name}</Card.Title>
                   <Card.Text className="league-show-card-text">
-                     Format: {league.league_format} <br/>
-                     Start: {formattedStart.toDateString()}<br/>
-                     End: {formattedEnd.toDateString()}<br/>
+                     <b>Format:</b> {league.league_format} <br/>
+                     <b>Start:</b> {formattedStart.toDateString()}<br/>
+                     <b>End:</b> {formattedEnd.toDateString()}<br/>
                   </Card.Text>
                </Card.Body>
+                  <ButtonGroup>
+                     <Button className="mr-2" variant="success">
+                        Edit League
+                     </Button>
+                     <Button variant="danger">
+                        Delete League
+                     </Button>
+                  </ButtonGroup>
             </Card>
             }
       </>
